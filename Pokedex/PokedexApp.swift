6fe4586be7563965a8baa0943a2cdc47029e3f25
GaldinoAllan <galdinoallancodes@gmail.com>
2025-06-09 10:52:59 -0300

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Allan Galdino on 09/06/25.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PokedexTests/PokedexTests.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexTests.swift
+//  PokedexTests
+//
+//  Created by Allan Galdino on 09/06/25.
+//
+
+import Testing
+@testable import Pokedex
+
+struct PokedexTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
